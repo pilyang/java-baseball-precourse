102917ff4a128ecb7104f525490f0da2ca48c78f
양재필 <pilyang@Pils-MacBookPro.local>
2022-10-11 16:04:07 +0900

    feat: verify strikes

--- src/main/java/baseball/component/Referee.java
+++ src/main/java/baseball/component/Referee.java
@@ -2,20 +2,34 @@ package baseball.component;
 
 public class Referee {
 
-    private int[] computerNumber;
-    private int[] playerNumber;
+    private int[] computerNumbers;
+    private int[] playerNumbers;
 
     private int strikes;
     private int balls;
 
     public Referee(){ }
 
-    public void setComputerNumber(int[] computerNumber){
-        this.computerNumber = computerNumber;
+    public void playerTurn(int[] playerNumbers){
+        setPlayerNumber(playerNumbers);
     }
 
-    public void setPlayerNumber(int[] playerNumber){
-        this.playerNumber = playerNumber;
+    private void verifyStrikes(){
+       int count = 0;
+       for(int i=0; i<3; i++){
+           if(computerNumbers[i] == playerNumbers[i]){
+               count++;
+           }
+       }
+       strikes = count;
+    }
+
+    public void setComputerNumbers(int[] computerNumbers1s){
+        this.computerNumbers = computerNumbers;
+    }
+
+    public void setPlayerNumber(int[] playerNumbers){
+        this.playerNumbers = playerNumbers;
     }
 
 
